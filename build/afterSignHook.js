6271459f51f68c9fc2a7512b43098c45aec60446
Jp <jpsiegel8@gmail.com>
2022-10-14 20:47:55 -0400

    v1.0.7

---
+++ build/afterSignHook.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+const fs = require("fs");
+const path = require("path");
+var electron_notarize = require("electron-notarize");
+
+module.exports = async function (params) {
+  if (process.platform !== "darwin") {
+    return;
+  }
+
+  console.log("afterSign hook triggered", params);
+
+  let appId = "ptg.launcher-kr.app";
+
+  let appPath = path.join(
+    params.appOutDir,
+    `${params.packager.appInfo.productFilename}.app`
+  );
+  if (!fs.existsSync(appPath)) {
+    console.log("skip");
+    return;
+  }
+
+  console.log(`Notarizing ${appId} found at ${appPath}`);
+
+  try {
+    await electron_notarize.notarize({
+      appBundleId: appId,
+      appPath: appPath,
+      appleApiKey: process.env.API_KEY,
+      appleApiKeyId: process.env.API_KEY_ID,
+      appleApiIssuer: process.env.API_KEY_ISSUER_ID,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+
+  console.log(`Done notarizing ${appId}`);
+};
